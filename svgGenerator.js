7779af957e25ff43b3f9cf343bd30113cfeb930c
Nana Elie <91574567+nanaelie@users.noreply.github.com>
2025-05-10 18:53:19 +0000

    Update svgGenerator.js

--- svgGenerator.js
+++ svgGenerator.js
@@ -2,13 +2,13 @@ function generateSVG(label, count, color) {
   const labelText = label;
   const countText = count.toString();
 
-  const labelWidth = labelText.length * 7 + 10;
-  const countWidth = countText.length * 7 + 10;
+  const labelWidth = labelText.length * 7 + 6;
+  const countWidth = countText.length * 7 + 6;
   const totalWidth = labelWidth + countWidth;
   const height = 20;
 
   return `
-<svg xmlns="http://www.w3.org/2000/svg" width="${totalWidth}" height="${height}" style="border-radius: 6px; ;">
+<svg xmlns="http://www.w3.org/2000/svg" width="${totalWidth}" height="${height}" style="border-radius: 6px;">
   <rect x="0" y="0" width="${labelWidth}" height="${height}" fill="#555" />
   <rect x="${labelWidth}" y="0" width="${labelWidth}" height="${height}" fill="${color}" /> // labelWidth
 
